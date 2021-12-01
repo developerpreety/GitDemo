1cfdabef59526c9515cec9f70e6010cb1c8afb3b
developerpreety <preetyverma5555@gmail.com>
2021-12-01 18:06:22 +0530

    New method is added in this commit.

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -5,6 +5,6 @@ public class GitDemo {
     }
     public static void main(String[] args) {
         System.out.println("Hey I'm Git Hub");
-        System.out.println(getN(7));
+        System.out.println(10+getN(7));
     }
 }
