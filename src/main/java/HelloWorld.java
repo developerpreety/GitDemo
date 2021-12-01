23ee943cef65a883113669d3b4ea27f3f0f48e92
developerpreety <95027006+developerpreety@users.noreply.github.com>
2021-12-02 00:24:46 +0530

    Update this new line

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first git commit");
+        System.out.println("Hello world");
+        
     }
 }
